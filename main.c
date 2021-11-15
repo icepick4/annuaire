5d37f6d06f940d65f33ae7ac0c7948ad799d8fdd
Nabile Sall <idrissa.sall@etu.univ-lyon1.fr>
2021-11-15 21:55:45 +0100

    Merge branch 'main' of https://forge.univ-lyon1.fr/p2101879/sae-c
    # Conflicts:
    #	main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAILLE_MAX 256
+int insertion(int t[],int len_t); /*initialisation de la fonction d'algorithme de tri*/
+int insertion (int t[],int len_t){
+    int i,h,c;
+    for(i=1;i<len_t;i++){
+        while((i>0) && (t[i]<t[i-1])){
+            c=t[i];
+            t[i]=t[i-1];
+            t[i-1]=c;
+            i=i-1;
+        }
+    }
+    for (h = 0; h < len_t; h++) {
+      printf("\ntableau[%d] = %d", h, t[h]);
+   }
+    return t;
+}
+int main(){
+    int tab[7] = {25,13,2,14,36,15,1};/* initialisation du tableau de valeurs*/
+    insertion(tab,(sizeof tab / sizeof tab[0]));
+    return 0;
+}
