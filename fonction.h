da9d7187142e610be567cf47c33199ac5cdcf17d
p2101879 <remi.jara@etu.univ-lyon1.fr>
2021-11-18 11:53:18 +0100

    ajout fonction afficher, supprimer (en cours), ajout clients, est_num pour détecter si num ou pas)

--- fonction.h
+++ fonction.h
@@ -1,2 +1,6 @@
 void insertion(int t[],int len_t); /*initialisation de la fonction d'algorithme de tri*/
-
+void ajout(char *nom, char *prenom, char *adresse, char *code_postale, char *num, char *mail, char *profession);
+void afficher();
+void suppr(char * num);
+int est_num(char *num);
+char * strtok_empty (char * str, char const * sep);
--- main.c
+++ main.c
@@ -1,11 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "fonction.c"
 #define TAILLE_MAX 256
+
 int main(){
     int tab[7] = {25,13,2,14,36,15,1};/* initialisation du tableau de valeurs*/
-    insertion(tab,(sizeof tab / sizeof tab[0]));
-    return 0;
+    /*insertion(tab,(sizeof tab / sizeof tab[0]));*/
+    int numeros[10] = {0,1,3,4,6,7,9,10,12,13};
+    char choix[TAILLE_MAX+1],nom[TAILLE_MAX+1],prenom[TAILLE_MAX+1],adresse[TAILLE_MAX+1],code_postale[TAILLE_MAX+1],num[TAILLE_MAX+1],mail[TAILLE_MAX+1],profession[TAILLE_MAX+1];
+    printf("Choisi une action à réaliser : Ajout de client (\"ajout\"), Afficher la base de données (\"afficher\"), Supprimer un client (\"suppr\")");
+    scanf("%s",&choix);
+    if (strcmp(choix, "afficher")==0){
+        /*ajouter le tri*/
+        afficher();
+    }
+    else if (strcmp(choix, "ajout")==0){
+            scanf("%s%s%s%s%s%s%s",&nom,&prenom,&adresse,&code_postale,&num,&mail,&profession);
+            int i;
+            ajout(nom,prenom,adresse,code_postale,num,mail,profession);
+    }
+    else if(strcmp(choix,"suppr")==0){
+        scan_num : printf("Entrez le numéro de téléphone du client à supprimer");
+        char num[TAILLE_MAX+1];
+        scanf("%s",&num);
+        if(est_num(num)){
+            suppr(num);
+        }
+        else{
+            goto scan_num;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
+
+
+
+
+
+
 
 
