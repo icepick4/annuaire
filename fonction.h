0a3eef25c214e58c8d4a9a0e6a7f8ed4fff3e753
p2101879 <remi.jara@etu.univ-lyon1.fr>
2021-11-23 09:56:27 +0100

    structures, fonction supprimer, ajouter, afficher, (attention warning dans le remplissage du fichier sur les strdup)

--- fonction.h
+++ fonction.h
@@ -1,6 +1,9 @@
 void insertion(int t[],int len_t); /*initialisation de la fonction d'algorithme de tri*/
 void ajout(char *nom, char *prenom, char *adresse, char *code_postale, char *num, char *mail, char *profession);
-void afficher();
-void suppr(char * num);
+void afficher(char *choix_tri);
+int suppr(int supprligne);
+int trouver (char * scanprenom,char * scannom,char * scannum, char *scanmail,int checksuppr);
 int est_num(char *num);
 char * strtok_empty (char * str, char const * sep);
+int est_mail(char *mail);
+
--- main.c
+++ main.c
@@ -3,36 +3,93 @@
 #include <string.h>
 #include "fonction.c"
 #define TAILLE_MAX 256
-
-int main(){
+void main(){
+    remplir(0,0,-1);
     int tab[7] = {25,13,2,14,36,15,1};/* initialisation du tableau de valeurs*/
     /*insertion(tab,(sizeof tab / sizeof tab[0]));*/
-    int numeros[10] = {0,1,3,4,6,7,9,10,12,13};
-    char choix[TAILLE_MAX+1],nom[TAILLE_MAX+1],prenom[TAILLE_MAX+1],adresse[TAILLE_MAX+1],code_postale[TAILLE_MAX+1],num[TAILLE_MAX+1],mail[TAILLE_MAX+1],profession[TAILLE_MAX+1];
-    printf("Choisi une action à réaliser : Ajout de client (\"ajout\"), Afficher la base de données (\"afficher\"), Supprimer un client (\"suppr\")");
+    char choix[TAILLE_MAX+1],choix_tri[TAILLE_MAX+1],nom[TAILLE_MAX+1],prenom[TAILLE_MAX+1],adresse[TAILLE_MAX+1],code_postale[TAILLE_MAX+1],num[TAILLE_MAX+1],mail[TAILLE_MAX+1],profession[TAILLE_MAX+1];
+    debut : printf("\nChoisis une action a realiser : \n  - Ajout de client --> tapez \"ajout\" \n  - Afficher la base de donnees --> tapez \"afficher\" \n  - Supprimer un client --> tapez \"suppr\"\n");
     scanf("%s",&choix);
     if (strcmp(choix, "afficher")==0){
         /*ajouter le tri*/
-        afficher();
+        printf("Voulez vous trier l'affichage des données ? \n Si oui tapez \"\" pour ... \"\" pour ... sinon tapez \"non\"(en cours) ");
+        scanf("%s",choix_tri);
+        afficher(choix_tri);
     }
     else if (strcmp(choix, "ajout")==0){
-            scanf("%s%s%s%s%s%s%s",&nom,&prenom,&adresse,&code_postale,&num,&mail,&profession);
-            int i;
-            ajout(nom,prenom,adresse,code_postale,num,mail,profession);
+        printf("Prenom : ");
+        scanf("%s",&prenom);
+        printf("Nom : ");
+        scanf("%s",&nom);
+        printf("Ville : ");
+        scanf("%s",&adresse);
+        scan_ajout_codepostale : printf("Code postale : ");
+        scanf("%s",&code_postale);
+        if (!(est_code_postal(code_postale))){
+            printf("\nCode postale non valide\nNorme des codes postaux : 00000\n");
+            goto scan_ajout_codepostale;
+        }
+        scan_ajout_num : printf("Numero de telephone : ");
+        scanf("%s",&num);
+        if (!(est_num(num))){
+            printf("\nNumero de telephone non valide\nNorme des numeros de telephone : 00.00.00.00.00\n");
+            goto scan_ajout_num;
+        }
+        scan_ajout_mail : printf("Adresse email : ");
+        scanf("%s",&mail);
+        if (!(est_mail(mail))){
+            printf("\nEmail non valide\nNorme des adresses email : ---@---.---\n");
+            goto scan_ajout_mail;
+        }
+        printf("Profession : ");
+        scanf("%s",&profession);
+        ajout(nom,prenom,adresse,code_postale,num,mail,profession);
+        printf("Le client a bien ete ajoute\n");
+        goto debut;
     }
     else if(strcmp(choix,"suppr")==0){
-        scan_num : printf("Entrez le numéro de téléphone du client à supprimer");
-        char num[TAILLE_MAX+1];
-        scanf("%s",&num);
-        if(est_num(num)){
-            suppr(num);
+        char verif[TAILLE_MAX+1];
+        char num[TAILLE_MAX+1],mail[TAILLE_MAX+1],nom[TAILLE_MAX+1],prenom[TAILLE_MAX+1];
+        scan_nom : printf("Entrez le prenom du client a supprimer : ");
+        scanf("%s",&prenom);
+        printf("Entrez le nom du client a supprimer : ");
+        scanf("%s",&nom);
+        scan_critere : printf("Souhaitez vous entrer son email ou son numero de telephone ? \nPour l'email--> tapez \"mail\"\nPour le numero de telephone --> tapez \"tel\"\n");
+        scanf("%s",&verif);
+        if (strcmp(verif,"mail")==0){
+            scanmail : printf("Entrez le mail du client a supprimer : ");
+            scanf("%s",&mail);
+            if (est_mail(mail)){
+                trouver(prenom,nom,"0",mail,1);
+                goto debut;
+            }
+            else{
+                printf("\nEmail non valide\nNorme des adresses email : ---@---.---\n");
+                goto scanmail;
+            }
+        }
+        else if (strcmp(verif,"tel")==0){
+            scannum : printf("Entrez le numero de telephone du client a supprimer : ");
+            scanf("%s",&num);
+            if (est_num(num)){
+                trouver(prenom,nom,num,"0",1);
+                goto debut;
+            }
+            else{
+                printf("\nNumero de telephone non valide\nNorme des numeros de telephone : 00.00.00.00.00\n");
+                goto scannum;
+            }
         }
         else{
-            goto scan_num;
+            printf("\nErreur dans la selection\n");
+            goto scan_critere;
         }
     }
-
-    return EXIT_SUCCESS;
+    else if(strcmp(choix,"stop")==0){
+    }
+    else{
+        goto debut;
+    }
 }
 
 
